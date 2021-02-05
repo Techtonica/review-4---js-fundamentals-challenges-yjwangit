5a46af9afee7f1553a2e490770a025ffb627a9d0
Yingjie Wang <76074238+yjwangit@users.noreply.github.com>
2021-02-05 15:34:50 -0600

    calculate the minutes

--- challenges/review/4__js-fundamentals/4.js
+++ challenges/review/4__js-fundamentals/4.js
@@ -1,3 +1,12 @@
 // Create a function that calculates how many minutes have elapsed from midnight until right now.
 // The function should still return an accurate answer,
 // even if you ran it a few minutes or hours later.
+
+function mincal(){
+  let midnight = new Date("00:00:00 2021-02-05");
+  let timenow = new Date();
+  let timediff = timenow.getTime()-midnight.getTime();
+  let mindiff = parseInt(dimediff/1000/60);
+  return mindiff;
+}
+consolel.log(mincal());
