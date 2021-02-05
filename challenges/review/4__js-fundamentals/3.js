8cc29be92dce31f2125d669cd0269da30c4efa5f
Yingjie Wang <76074238+yjwangit@users.noreply.github.com>
2021-02-05 14:39:36 -0600

    array

--- challenges/review/4__js-fundamentals/3.js
+++ challenges/review/4__js-fundamentals/3.js
@@ -5,3 +5,10 @@
 //   - Add a new item to the end of the array (ex: [1,2,3,4])
 //   - Add an array to the beginning of the array. (ex: [["a","b","c"],1,2,3,4])
 //   - Return the first item from the first item in the array. (ex: "a")
+
+let arr = [1,2,3,4,5,6,7,8,9,90];
+return arr[0];
+return arr[arr.length-1];
+arr.push(100);
+arr.unshift(["apple",88,"fish"]);
+return arr[0][0];
